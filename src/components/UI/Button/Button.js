e5f85356e45ccdae837e4343a46323a5d179dd2d
Andres Mena <ballerlikemike23@gmail.com>
2018-08-05 00:40:29 -0400

    Implementing the Button Component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,9 +3,10 @@ import React from 'react'
 import classes from './Button.css'
 
 const button = (props) => (
-  <button>
+  <button
     className={[classes.Button, classes[props.btnType]].join(' ')}
     onClick={props.clicked}
+  >
     {props.children}
   </button>
 )
