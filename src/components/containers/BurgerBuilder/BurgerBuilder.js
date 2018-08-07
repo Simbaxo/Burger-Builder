1db33eab158907612c2dce18a14d202999954b8a
Andres Mena <ballerlikemike23@gmail.com>
2018-08-06 23:00:46 -0400

    Sending a POST Request

--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../Burger/Burger'
 import BuildControls from '../../Burger/BuildControls/BuildControls'
 import Modal from '../../UI/Modal/Modal'
 import OrderSummary from '../../Burger/OrderSummary/OrderSummary'
+import axios from '../../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -82,7 +83,28 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!')
+    // alert('You continue!')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Andres Mena',
+        address: {
+          street: 'Teststreet 1',
+          zipCode: '22312',
+          country: 'Germany'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/orders.json', order)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   render() {
