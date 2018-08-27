ccb529bc222ec249ca7dd358a5dadf7fe87f135f
Andres Mena <ballerlikemike23@gmail.com>
2018-08-27 18:47:26 -0400

    Update Folder Structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-import Aux from '../../../hoc/Aux/Aux'
-import Burger from '../../Burger/Burger'
-import BuildControls from '../../Burger/BuildControls/BuildControls'
-import Modal from '../../UI/Modal/Modal'
-import OrderSummary from '../../Burger/OrderSummary/OrderSummary'
-import Spinner from '../../UI/Spinner/Spinner'
-import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
-import axios from '../../../axios-orders'
+import Aux from '../../hoc/Aux/Aux'
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
