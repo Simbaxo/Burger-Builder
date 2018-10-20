adb2f372983670b94e9e7608c4eb16c0d8270723
Andres Mena <ballerlikemike23@gmail.com>
2018-10-19 23:58:19 -0400

    Logging Users Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,6 +22,19 @@ export const authFail = (error) => {
   }
 }
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout())
+    }, expirationTime * 1000);
+  }
+}
+
 export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart())
@@ -38,6 +51,7 @@ export const auth = (email, password, isSignup) => {
       .then(res => {
         console.log(res)
         dispatch(authSuccess(res.data.idToken, res.data.localId))
+        dispatch(checkAuthTimeout(res.data.expiresIn))
       })
       .catch(err => {
         dispatch(authFail(err.response.data.error))
